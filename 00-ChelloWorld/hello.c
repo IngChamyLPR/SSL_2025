d7be324116d6b6810869d643fbb9f2b7215851b9
IngChamyLPR <plauture@frba.utn.edu.ar>
2025-04-16 16:18:36 -0300

    Subiendo TP0

---
+++ 00-ChelloWorld/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char nombre[] = "Pierre";
+    char apellido[] = "LAUTURE";
+
+    printf("Hello, %s %s!\n", nombre, apellido);
+
+    return 0;
+}
